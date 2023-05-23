7b2743b2f8c0f50f3debad9e9781e0c812493e97
boboan <360969885@qq.com>
2023-05-23 19:46:20 +0800

    init: Response

--- api/src/blog/post.rs
+++ api/src/blog/post.rs
@@ -1,7 +1,54 @@
 use crate::AppState;
-use axum::extract::State;
+use axum::{
+    extract::State,
+    http::{header, HeaderName, HeaderValue},
+    response::{AppendHeaders, IntoResponse},
+    Json,
+};
+use serde::Serialize;
+use serde_json::json;
 
-pub async fn index_handler(state: State<AppState>) -> &'static str {
-    println!("{:?}", state);
-    "Hello Posts Index"
+pub async fn index_handler(state: State<AppState>) -> impl IntoResponse {
+    let res_json = ResJson {
+        code: 200,
+        data: "123".into(),
+        message: "success".into(),
+    };
+
+    res_json
+}
+
+// pub async fn index_handler(state: State<AppState>) -> impl IntoResponse {
+//     let res_json = ResJson {
+//         code: 200,
+//         data: "123".into(),
+//         message: "success".into(),
+//     };
+
+//     let json_string = json!(res_json).to_string();
+
+//     // add header AppendHeaders
+
+//     let header_part = AppendHeaders([(
+//         header::CONTENT_TYPE,
+//         HeaderValue::from_str("application/json").unwrap(),
+//     )]);
+
+//     // (part,intoresponse)
+//     (header_part, json_string)
+// }
+
+#[derive(Debug, Serialize)]
+struct ResJson {
+    code: i32,
+    data: String,
+    message: String,
+}
+
+impl IntoResponse for ResJson {
+    fn into_response(self) -> axum::response::Response {
+        let val = json!(self);
+
+        Json(val).into_response()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use api;
 use dotenvy;
+use serde::Serialize;
+use serde_json::json;
 
 fn main() {
-    dotenvy::dotenv().expect(".env 文件读取失败");
-    api::start();
+        dotenvy::dotenv().expect(".env 文件读取失败");
+        api::start();
+
+    // let res_json = ResJson {
+    //     code: 200,
+    //     data: "123".into(),
+    //     message: "success".into(),
+    // };
+
+    // let json_string = json!(res_json).to_string();
+
+    // println!("{json_string}");
+}
+
+#[derive(Debug, Serialize)]
+struct ResJson {
+    code: i32,
+    data: String,
+    message: String,
 }
